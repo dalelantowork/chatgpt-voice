8183e6ec3252866d5b7a56bbeaf89b0dd5707f45
Dale Lanto <dalelanto.work@gmail.com>
2023-09-09 00:23:46 +0800

    Upload Project

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-# Voice GPT (voice-gpt)
+# voice-gpt
 
-## Click on the image to go to the Youtube tutorial!
+This template should help get you started developing with Vue 3 in Vite.
 
-[![GO TO JOHN WEEKS DEV TUTORIAL VIDEOS](https://user-images.githubusercontent.com/108229029/258213516-51adce2c-c05c-44c0-8415-27310ec41592.png)](https://www.youtube.com/watch?v=8qi_CrIBNNI)
+## Recommended IDE Setup
 
-Come and check out my YOUTUBE channel for lots more tutorials -> https://www.youtube.com/@johnweeksdev
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-LIKE, SUBSCRIBE, and SMASH THE NOTIFICATION BELL!!!
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({ apiKey: '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'});
-const openai = new OpenAIApi(configuration);
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+  apiKey: "OPENAI_API_KEY_HERE"
+});
 
 const AWS = require("aws-sdk");
 AWS.config.loadFromPath("awsCreds.json");
@@ -14,39 +16,40 @@ AWS.config.loadFromPath("awsCreds.json");
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/api/text-to-audio-file', async (req, res) => {
+app.post("/api/text-to-audio-file", async (req, res) => {
+  const completion = await openai.completions.create({
+    model: "text-davinci-003",
+    prompt: req.body.text,
+    max_tokens: 100,
+    temperature: 0.5,
+  });
 
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: req.body.text,
-        max_tokens: 100,
-        temperature: 0.5
-    })
+  let num = (Math.random() * 100000000).toFixed(0);
 
-    let num = (Math.random() * 100000000).toFixed(0);
+  const polly = new AWS.Polly({ region: "us-east-1" });
+  const params = {
+    OutputFormat: "mp3",
+    Text: completion.choices[0].text,
+    VoiceId: "Matthew",
+  };
 
-    const polly = new AWS.Polly({ region: "us-east-1" })
-    const params = {
-        OutputFormat: "mp3",
-        Text: completion.data.choices[0].text,
-        VoiceId: "Matthew"
+  polly.synthesizeSpeech(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
     }
 
-    polly.synthesizeSpeech(params, (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+    let filePath = "../public/voice/";
+    let fileName = num + ".mp3";
 
-        let filePath = "../public/voice/";
-        let fileName = num + ".mp3";
+    if (num) fs.writeFileSync(filePath + fileName, data.AudioStream);
+  });
 
-        if (num) fs.writeFileSync(filePath + fileName, data.AudioStream)
-    })
+  setTimeout(() => {
+    res.status(200).json(num);
+  }, 4500);
+});
 
-    setTimeout(() => { res.status(200).json(num) }, 4500)
-})
-
-app.listen(4001, () => { 
-    console.log(`Server is ready at http://localhost:4001`); 
+app.listen(4001, () => {
+  console.log(`Server is ready at http://localhost:4001`);
 });
